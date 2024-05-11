443e5cc3fc40d53078265de7d3739e62101109e1
AlxcNL <J.A.Boogaard@hr.nl>
2024-05-12 00:25:59 +0200

    Bugfix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker buildx build --no-cache --load --build-arg PLATFORM=arm64 -t torcs-server:0.1 -f Dockerfile.aarch64 .
+docker-buildx build --no-cache --load --build-arg PLATFORM=arm64 -t torcs-server:0.1 -f Dockerfile.aarch64 .
