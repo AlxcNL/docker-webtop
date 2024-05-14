7e34f89d05868fe606e32caf9bce70ae0c7fc02f
AlxcNL <J.A.Boogaard@hr.nl>
2024-05-14 09:27:40 +0200

    Added docker repo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$(uname -m)" -eq "x86_64" ]]; then
-    docker-buildx build $1 --load --build-arg PLATFORM=amd64 -t torcs-server:0.1 -f Dockerfile .
+    docker-buildx build $1 --load --build-arg PLATFORM=amd64 -t jaboo/torcs-server:0.1 -f Dockerfile .
 else
-    docker-buildx build $1 --load --build-arg PLATFORM=arm64 -t torcs-server:0.1 -f Dockerfile.aarch64 .
+    docker-buildx build $1 --load --build-arg PLATFORM=arm64 -t jaboo/torcs-server:0.1 -f Dockerfile.aarch64 .
 fi
