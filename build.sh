8792280485a4c357fd9dfa7c7ecb5ee9945c11b2
AlxcNL <J.A.Boogaard@hr.nl>
2024-05-27 15:58:01 +0200

    Improved build script

--- build.sh
+++ build.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env bash
+# Author: J.A.Boogaard@hr.nl
 
-image="jaboo/torcs-server:0.1"
-action="load"
+repo="jaboo"
+image_name="torcs-server"
+tag="0.1"
+image="${repo}/${image_name}:${tag}"
 arch=$(uname -m)
 
-[[ -z $1 ]] || action=$1
-
-printf "%s image %s for %s\n" $action $image $arch
-
-if [[ "$arch" -eq "x86_64" ]]; then
-    docker buildx build "--${action}" --build-arg PLATFORM=amd64 -t $image -f Dockerfile .
+if [[ -n $1 ]]; then
+    action=$1
 else
-    docker buildx build "--${action}" --build-arg PLATFORM=arm64 -t $image -f Dockerfile.aarch64 .
+    action="load"
+fi
+
+function buildImage() {
+    printf "Build and %s image %s for %s\n" $action $image $arch;
+
+    if [[ "$arch" -eq "x86_64" ]]; then
+        cmd="docker buildx build --${action} --build-arg PLATFORM=amd64 -t $image -f Dockerfile ."
+    else
+        cmd="docker buildx build --${action} --build-arg PLATFORM=arm64 -t $image -f Dockerfile.aarch64 ."
+    fi
+
+    echo $cmd
+    eval $cmd
+
+}
+
+buildImage
+
+if [[ $action -eq "push" ]]; then
+    image="${repo}/${image_name}:latest"
+    buildImage
 fi
