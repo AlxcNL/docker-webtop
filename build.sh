cca53e675545d4dbd2182bf55c0036fa2c2a3d40
git_config.sh <AlxcNL>
2024-05-28 20:57:57 +0200

    Updated docker configuration

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-image="jaboo/torcs-server:0.1"
+image="jaboo/torcs-server:0.2"
 action="load"
 arch=$(uname -m)
 
@@ -9,7 +9,7 @@ arch=$(uname -m)
 printf "%s image %s for %s\n" $action $image $arch
 
 if [[ "$arch" -eq "x86_64" ]]; then
-    docker buildx build "--${action}" --build-arg PLATFORM=amd64 -t $image -f Dockerfile .
+    docker buildx build --no-cache "--${action}" --build-arg PLATFORM=amd64 -t $image -f Dockerfile .
 else
-    docker buildx build "--${action}" --build-arg PLATFORM=arm64 -t $image -f Dockerfile.aarch64 .
+    docker buildx build --no-cache "--${action}" --build-arg PLATFORM=arm64 -t $image -f Dockerfile.aarch64 .
 fi
